3e7078bf9ff22bc74b5bb9e6c7c487615ba144a2
Nikolay Serduchenko <nik.serdu201584@gmail.com>
2026-01-08 00:17:55 +0300

    feat: add GetLocationsByUserId to CameraService

--- src/proto/paths.ts
+++ src/proto/paths.ts
@@ -2,5 +2,5 @@ import { join } from 'path'
 
 export const PROTO_PATHS = {
 	AUTH: join(__dirname, '../../proto/auth.proto'),
-	USER: join(__dirname, '../../proto/user.proto'),
+	CAMERA: join(__dirname, '../../proto/camera.proto'),
 } as const
