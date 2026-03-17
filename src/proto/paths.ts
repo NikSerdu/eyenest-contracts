dd0c798190b1943052bf373523bee2b74d56f4d5
Nikolay Serduchenko <nik.serdu201584@gmail.com>
2026-03-17 22:49:06 +0300

    chore: add VideoService with StartRecording method

--- src/proto/paths.ts
+++ src/proto/paths.ts
@@ -3,4 +3,5 @@ import { join } from 'path'
 export const PROTO_PATHS = {
 	AUTH: join(__dirname, '../../proto/auth.proto'),
 	CAMERA: join(__dirname, '../../proto/camera.proto'),
+	VIDEO: join(__dirname, '../../proto/video.proto'),
 } as const
