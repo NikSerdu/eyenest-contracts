1d05c234a06895cf3a5098aac247dc32bedf311c
Nikolay Serduchenko <nik.serdu201584@gmail.com>
2026-03-22 16:27:11 +0300

    feat: add Events proto

--- src/proto/paths.ts
+++ src/proto/paths.ts
@@ -4,4 +4,5 @@ export const PROTO_PATHS = {
 	AUTH: join(__dirname, '../../proto/auth.proto'),
 	CAMERA: join(__dirname, '../../proto/camera.proto'),
 	VIDEO: join(__dirname, '../../proto/video.proto'),
+	EVENTS: join(__dirname, '../../proto/events.proto'),
 } as const
